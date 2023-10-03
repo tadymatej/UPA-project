5ecc1fae9b20a36934a447db30f2d10056fe10e1
Jan Zimola <zimolajan798@gmail.com>
2023-10-03 16:56:30 +0200

    addding client

--- mongo_db.py
+++ mongo_db.py
@@ -1,20 +1,22 @@
 import json
 
 from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-# Define MongoDB connection details
-mongodb_host = "localhost"
-mongodb_port = 27017
-database_name = "your_database_name"  # Change to your desired database name
+uri = "mongodb+srv://upa:upa@cluster0.cq5pjvs.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+database_name = "upa"  # Change to your desired database name
 # Change to your desired collection name
-collection_name = "your_collection_name"
+collection_name = "records"
 
 # Load the JSON data
-with open("your_json_file.json", "r") as json_file:
+with open("zastupitelstvo.json", "r") as json_file:
     data = json.load(json_file)
 
 # Connect to MongoDB
-client = MongoClient(mongodb_host, mongodb_port)
 db = client[database_name]
 collection = db[collection_name]
 
