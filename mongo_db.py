e47b0a35274d1baa4411368df3997b19f77f22b2
Eva Micankova <81229113+xmican10@users.noreply.github.com>
2023-10-05 00:02:12 +0200

    Implementing UPSERT for mongoDB

--- mongo_db.py
+++ mongo_db.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from pymongo import MongoClient
 from pymongo.mongo_client import MongoClient
@@ -13,16 +14,32 @@ database_name = "upa"  # Change to your desired database name
 collection_name = "records"
 
 # Load the JSON data
-with open("zastupitelstvo.json", "r") as json_file:
+with open("zastupitelstvo_debug.json", "r") as json_file:
     data = json.load(json_file)
 
 # Connect to MongoDB
 db = client[database_name]
 collection = db[collection_name]
 
+# ALERT! For testing purpuses delete all data
+result= collection.delete_many({})
+
 # Insert each document into the MongoDB collection
 for document in data["data"]:
-    collection.insert_one(document)
+    timestamp = document["datetime"][:-6]
+    document["datetime"] = timestamp
+    
+    # Checking if the document already exists in the database
+    duplicate_document = collection.find_one({'code': document["code"], 'subject': document["subject"]})
+    if duplicate_document:
+        if duplicate_document["datetime"] < document["datetime"]:
+            # New document contains updated data; old document is updated
+            collection.update_one({'code': document["code"], 'subject': document["subject"]}, {"$set": document}, upsert=False)
+    else:
+        # The document does not yet exist in the database; it is inserted into the database
+        collection.insert_one(document)
+        
+    # collection.update_one({'code': document["code"], 'subject': document["subject"], 'datetime': {"$lt": timestamp}}, {"$set": document},upsert=True)
 
 # Close the MongoDB connection
 client.close()
